89b57903cf98462063ce75750a64515e43af3a34
kadhika2 <kadhika2@u.rochester.edu>
2025-02-27 16:11:15 -0500

    Set up schema to handle sections and assignments

--- app/schema.sql
+++ app/schema.sql
@@ -1,18 +1,35 @@
+DROP TABLE IF EXISTS assignment;
+DROP TABLE IF EXISTS section;
+DROP TABLE IF EXISTS class;
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS classes;
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
-CREATE TABLE class (
+CREATE TABLE IF NOT EXISTS class (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     student_id INTEGER NOT NULL,
     class_name TEXT NOT NULL,
     class_code TEXT,
-    class_proffesor TEXT,
     class_grade REAL,
-    FOREIGN KEY (student_id) REFERENCES user (id)
+    FOREIGN KEY (student_id) REFERENCES user (id) ON DELETE CASCADE
 );
+CREATE TABLE IF NOT EXISTS section (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    section_name TEXT NOT NULL,
+    section_weight REAL NOT NULL,
+    class_id INTEGER NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES class (id) ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS assignment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    assignment_name TEXT NOT NULL,
+    assignment_grade REAL,
+    class_id INTEGER NOT NULL,
+    section_id INTEGER NOT NULL, 
+    FOREIGN KEY (class_id) REFERENCES class (id) ON DELETE CASCADE,
+    FOREIGN KEY (section_id) REFERENCES section (id) ON DELETE CASCADE
+)
