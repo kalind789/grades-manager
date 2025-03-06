7f718d2d8359a7b0534d08806fe20d0fff6e9ab9
kadhika2 <kadhika2@u.rochester.edu>
2025-03-06 02:08:07 -0500

    changed url for backend

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:5000', // Change this to your backend URL
+    baseURL: 'https://grades-manager.onrender.com', // Change this to your backend URL
+    // baseURL: 'http://127.0.0.1:5000', // Change this to your backend URL
 });
 
 axiosInstance.interceptors.request.use(
